5b34f566c479c94e476ad6279f52bde2a4542ced
Wish_Time <wish-time@outlook.com>
2017-12-22 16:58:02 +0800

    Initial commit

---
+++ style/js/hftop.js
@@ -0,0 +1,13 @@
+﻿jQuery(document).ready(function ($) {
+     $('#gotop').click(function () { $('html,body').animate({ scrollTop: '0px' }, 800); });
+    $(window).scroll(function (event) {
+        if ($.browser.ie6) {
+            $('#gotop').css('top', $(this).scrollTop() + $(this).height() - 170);
+        }
+        if ($(this).scrollTop() > 260) {
+            $('#gotop').fadeIn();
+        } else {
+            $('#gotop').fadeOut();
+        }
+    });
+});
